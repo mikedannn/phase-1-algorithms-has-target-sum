f3f7e6336c8b00aa334c851db449b3a44117a2ae
Michael <michael.daniels1013@gmail.com>
2022-03-06 18:56:42 -0600

    commit tree 2c4085074df94801ce739904d6eb4493c7b9d79b

--- index.js
+++ index.js
@@ -1,17 +1,34 @@
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for(let startIndex = 0; startIndex < array.length - 1; startIndex++){
+    for(let j = startIndex + 1; j < array.length; j++){
+      if(array[startIndex] + array[j] === target){
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 /* 
-  Write the Big O time complexity of your function here
+  0(log n)
 */
 
 /* 
-  Add your pseudocode here
+  function hasTargetSum(array, target) {
+    for loop over the array incrementing over each integer
+      set a variable to the array position + 1 and have it increment up
+        if integer + integer = target
+          return true
+
+    return false 
+  }
+  
 */
 
 /*
-  Add written explanation of your solution here
+  The function hasTargetSum takes in an array and a target.  It loops over each integer in the array, 
+  and then sets a variable to another position in the array.  If adding the integer and the variable value = the target 
+  it will return true.  if not it returns false. 
 */
 
 // You can run `node index.js` to view these console logs
@@ -29,6 +46,18 @@ if (require.main === module) {
 
   console.log("Expecting: false");
   console.log("=>", hasTargetSum([1, 2, 5], 4));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", hasTargetSum([4, 7, 12, 9, 80, 7], 87));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", hasTargetSum([3, 12, 11, 7], 110));
+
+  console.log("");
 }
 
 module.exports = hasTargetSum;
